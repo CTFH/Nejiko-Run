1bf8c280a513921d10326ea85509f2667dd7603d
cf <hoge>
2022-02-09 12:32:00 +0900

    p303

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;　//書かないとテキスト型が認識できない
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    public void Update()
+    {
+        //スコアを更新
+        int score = CalcScore();
+        scoreText.text = $"Score:{score}m";
+        //"Score : " + score + "m";
+
+        //ライフパネルを更新
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore()
+    {
+        //ねじ子の走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+        //nejiko.=ネジココントローラー（型）
+        //正式には　NejikoController.GameObject.GetComponent<transform>();
+    }
+}
---
+++ Assets/Scripts/LifePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePanel : MonoBehaviour
+{
+    public GameObject[] icons;　//3つのアイコンを登録
+
+    //ライフに応じてスプライトを出しわける
+    public void UpdateLife(int life)　//life＝3
+    {
+        for (int i=0; i<icons.Length; i++)
+        {
+            if (i < life)
+            {
+                //SetActive関数はゲームオブジェクトの有効、無効の設定を切り替える関数
+                //そのゲームオブジェクトが有効か無効かを決めるメソッド
+                //超重要
+                icons[i].SetActive(true);
+            }
+            else
+            {
+                icons[i].SetActive(false);
+            }
+        }
+    }
+    
+}
