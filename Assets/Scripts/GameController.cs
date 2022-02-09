0fd0da9d88b644f57bcea0458a9a440a379db5a7
cf <hoge>
2022-02-09 13:51:18 +0900

    p311

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;　//書かないとテキスト型が認識できない
 
 public class GameController : MonoBehaviour
 {
-    public NejikoController nejiko;
-    public Text scoreText;
-    public LifePanel lifePanel;
+    public NejikoController nejiko; //Component
+    //Transform型 nejiko
+    //GameObject型 nejiko
+    public Text scoreText; //Component
+    public LifePanel lifePanel; //Component
 
     public void Update()
     {
@@ -15,9 +17,17 @@ public class GameController : MonoBehaviour
         int score = CalcScore();
         scoreText.text = $"Score:{score}m";
         //"Score : " + score + "m";
+        //ScoreTextはコンポーネントで　.textはプロパティ
 
         //ライフパネルを更新
         lifePanel.UpdateLife(nejiko.Life());
+        //ネジコがトランスフォーム型のとき
+        //nejiko.gameObject.GetComponent<NejikoController>().Life();
+
+        //GameObject型の時
+        //nejiko.GetComponent<NejikoController>().Life();
+
+        //.LifeはNejikoControllerのライフメソッド
     }
 
     int CalcScore()
@@ -25,6 +35,12 @@ public class GameController : MonoBehaviour
         //ねじ子の走行距離をスコアとする
         return (int)nejiko.transform.position.z;
         //nejiko.=ネジココントローラー（型）
-        //正式には　NejikoController.GameObject.GetComponent<transform>();
+        //正式には　NejikoController.gameObject.GetComponent<transform>(); ()の後は.propertyやメソッドなどがくる
+
+        //transform型
+        //nejiko.position.z;
+
+        //GameObject型の時
+        //nejiko.transform.position.z;
     }
 }
