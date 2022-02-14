4166d3342515cc3c6847851dc78499da9bb6d8ec
cf <hoge>
2022-02-14 11:01:42 +0900

    完成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;　//書かないとテキスト型が認識できない
+using UnityEngine.SceneManagement; //名前空間インポート
 
 public class GameController : MonoBehaviour
 {
@@ -28,6 +29,30 @@ public class GameController : MonoBehaviour
         //nejiko.GetComponent<NejikoController>().Life();
 
         //.LifeはNejikoControllerのライフメソッド
+
+        //ネジコのライフが0になったらゲームオーバー
+        if(nejiko.Life() <= 0)
+        {
+            //これ以降のUpdateは止める
+            enabled = false; //EnableはGameControllerのチェックを外す
+                             //（GameObjectの有効はSetActiveで設定）
+                             //通常GetComponent<>().enabledだけど、今回は自分自身なのでそのままenabled
+                             //setActiveはメソッド（なので（）が必要）、enableはプロパティ
+
+            //ハイスコアを更新
+            //データの保存はデータベース化ファイルに保存するかのどちらか
+            //ちょっとしたデータならローカルにファイルを保持
+            //PlayerPrefsがそれ（データの保存）に該当　もともと用意されているクラス
+            if (PlayerPrefs.GetInt("HighScore")<score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+                //setAttributeに似てる（"ラベル", 値）　
+            }
+
+            //2秒後にReturnToTitleを呼び出す
+            Invoke("ReturnToTitle", 2.0f);
+            //第一引数の関数を第二引数の時間数後に呼び出す
+        }
     }
 
     int CalcScore()
@@ -42,5 +67,11 @@ public class GameController : MonoBehaviour
 
         //GameObject型の時
         //nejiko.transform.position.z;
+    }
+
+    void ReturnToTitle()
+    {
+        //タイトルシーンに切り替え
+        SceneManager.LoadScene("Title");
     }
 }
